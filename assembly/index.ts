7cec4bcad91326b52c1c6e316365719d452234da
timothyAgevi <timothyagevi@gmail.com>
2022-05-23 17:26:27 +0300

    add gretting function

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,3 +1,13 @@
 export function helloWorld(): string {
     return 'Hello World';
     }
+   
+    /**
+ * Edit greeting() function such that it returns a string containing both name parameter and the wallet address calling this function
+ * Hint: wallet address is found as follows: Context.sender
+ * @param name
+ * @returns
+ */
+export function greeting(name: string): string {
+    return '${Content.name } of wallet ${Context.sender} greated you';
+  }
